1c65511be7a31e0d4c2a0be7b4e1b7f9654ac928
shirleiso28 <shib.shirlei@gmail.com>
2022-04-29 22:34:29 -0300

    Função seculo-ano

---
+++ seculo-ano.php
@@ -0,0 +1,25 @@
+<?php
+
+function SeculoAno($ano){
+	//separar os digitos em unidades dentro de um vetor
+	$unidade = str_split($ano, 1);
+
+	/*Verifica se dois ultimos digitos são 0
+	**Se sim, o século condiz com os 2 primeiros dígitos do ano
+	**Senão, o século condiz com os 2 primeiros dígitos do ano + 1
+	*/
+	if($unidade[2] == 0 && $unidade[3] == 0){
+		$seculo = $unidade[0].$unidade[1];
+	}else{
+
+		$seculo = $unidade[0].$unidade[1];
+		$seculo = intval($seculo) + 1; //Converte para inteiro para poder realizar a soma
+	}
+
+	echo "Ano ".$ano." = século ".$seculo;
+
+}
+
+//Chamada de teste da função
+SeculoAno(1905);
+?>
